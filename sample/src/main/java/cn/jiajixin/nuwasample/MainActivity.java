62467e7636c23bb35ee7ec7b345ab84cd3d4bbf5
yanglin <yanglin@115.com>
2015-11-17 17:47:36 +0800

    UI

--- sample/src/main/java/cn/jiajixin/nuwasample/MainActivity.java
+++ sample/src/main/java/cn/jiajixin/nuwasample/MainActivity.java
@@ -2,7 +2,10 @@ package cn.jiajixin.nuwasample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.inputmethod.EditorInfo;
 import android.widget.TextView;
 
 import cn.jiajixin.nuwasample.Hello.Hello;
@@ -17,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         TextView textView = (TextView) findViewById(R.id.textview);
         textView.setText(new Hello().say());
+
+        AppCompatEditText editText = (AppCompatEditText) findViewById(R.id.edt_content);
+        editText.setInputType(EditorInfo.TYPE_CLASS_NUMBER);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
